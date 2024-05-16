4d68055996793dd588159eeb58a62539fe673f2d
Matthew <kagzgenius@gmail.com>
2024-05-16 17:27:41 -0500

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://kagzgenius.github.io";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -28,8 +28,8 @@ if (isBuild) {
 }
 
 export default defineConfig({
-  server: { port: SERVER_PORT },
-  site: BASE_URL,
+  // server: { port: SERVER_PORT },
+  site: LIVE_URL,
   integrations: [
     sitemap(),
     tailwind({
